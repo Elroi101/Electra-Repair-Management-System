d3afc7242f4052bdbe863514fcc606a73ebc66fa
Elroi101 <149605457+Elroi101@users.noreply.github.com>
2026-07-29 22:13:41 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,8 @@ const transporter = nodemailer.createTransport({
 });
 
 const db = new pg.Pool({
-  // eslint-disable-next-line no-undef
-  user: process.env.DB_USER,
-  // eslint-disable-next-line no-undef
-  host: process.env.DB_HOST,
-  // eslint-disable-next-line no-undef
-  database: process.env.DB_NAME,
-  // eslint-disable-next-line no-undef
-  password: process.env.DB_PASSWORD,
-  // eslint-disable-next-line no-undef
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 const saltrounds = 10;
